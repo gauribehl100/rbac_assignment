9514b45daaef89c23a9b707e3cf0031287035d64
Gauri Behl <140520216+gauribehl100@users.noreply.github.com>
2025-04-23 15:04:22 +0530

    HomePg.jsx

--- src/Dashboard/HomePg.jsx
+++ src/Dashboard/HomePg.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import Chart from './Chart';
 import ProgressBar from './ProgressBar';
@@ -5,26 +6,20 @@ import ItemLists from './ItemLists';
 
 function HomePg() {
     return (
-        <div className="grid grid-cols-[17%_83%] md:grid-cols-1">
+        <div className="min-h-screen bg-blue-100 p-4">
            
+            <div className="flex flex-wrap justify-center gap-6 py-4">
+                <ItemLists type="user" />
+                <ItemLists type="orders" />
+                <ItemLists type="products" />
+                <ItemLists type="balance" />
+                <ItemLists type="revenue" />
+            </div>
 
-            <div className="relative">
-             
-
-                <div className="bg-blue-200 h-40 md:h-24 rounded-b-2xl" />
-                <div className="grid grid-cols-4 gap-5 p-6 -mt-20 md:grid-cols-3 sm:grid-cols-2 sm:-mt-10">
-                    <ItemLists type="user" />
-                    <ItemLists type="orders" />
-                    <ItemLists type="products" />
-                    <ItemLists type="balance" />
-                    <ItemLists type="revenue" />
-                </div>
-
-                <div className="flex justify-between gap-16 ">
-                    <ProgressBar />
-                    <Chart height={450} title="Revenue" />
-                </div>
-
+            
+            <div className="flex flex-col xl:flex-row justify-between items-center gap-8 py-4">
+                <ProgressBar />
+                <Chart height={400} title="Revenue" />
             </div>
         </div>
     );
