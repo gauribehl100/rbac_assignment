bc3356ac1fd644db06cf30797e9724dabe3f974c
Gauri Behl <140520216+gauribehl100@users.noreply.github.com>
2025-04-23 15:00:57 +0530

    RolesTable.jsx

--- src/Roles_Permissions/RolesTable.jsx
+++ src/Roles_Permissions/RolesTable.jsx
@@ -33,60 +33,63 @@ function RolesTable({roleData,deleteRoleHandler, editRoleHandler }) {
   
 
   return (
-    <div>
-      <div>
-      <Link to="/rolesform">
+    <div >
+     <div className='flex justify-end pr-20'>
+     <Link to="/rolesform">
         <button
-          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 mt-7 ml-[67rem]"
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 mt-7 ml-auto sm:ml-4 md:ml-8 lg:ml-16 xl:ml-[67rem]"
+
         >
           Add New Role
         </button>
       </Link>
-<input
-id="search"
-type="text"
-className="px-4 py-2 border rounded-lg w-64 ml-2"
-placeholder="Enter role for search "
-/>
+     </div>
 
-</div>
+<TableContainer component={Paper} sx={{ mt: 3, width: '97%', ml: 3 }}>
+  <Table>
+    <TableHead>
+      <TableRow>
+        <TableCell align="center" sx={{ width: '25%' }}>Role</TableCell>
+        <TableCell align="center" sx={{ width: '20%' }}>Status</TableCell>
+        <TableCell align="center" sx={{ width: '25%' }}>Created On</TableCell>
+        <TableCell align="center" sx={{ width: '25%' }}>Actions</TableCell>
+      </TableRow>
+    </TableHead>
+    <TableBody>
+      {roleData.map((row, index) => (
+        <TableRow key={index}>
+          <TableCell align="center">{row.role}</TableCell>
+          <TableCell align="center">
+            <StatusButton status={row.status} />
+          </TableCell>
+          <TableCell align="center">{row.createdOn}</TableCell>
+          <TableCell
+            align="center"
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height:'70px'
+            }}
+          >
+            <Link to={'/rolesform'}>
+              <ModeEditIcon
+                sx={{ cursor: 'pointer', marginRight: 2 }}
+                onClick={() => editRoleHandler(row)}
+              />
+            </Link>
 
-      <TableContainer component={Paper} sx={{ mt: 3, width: '97%', ml: 3 }}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell align="center">Role</TableCell>
-              <TableCell align="center">Status</TableCell>
-              <TableCell align="center">Created On</TableCell>
-              <TableCell align="center">Actions</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {roleData.map((row, index) => (
-              <TableRow key={index}>
-                <TableCell align="center">{row.role}</TableCell>
-                <TableCell align="center">
-                  <StatusButton status={row.status} />
-                </TableCell>
-                <TableCell align="center">{row.createdOn}</TableCell>
-                <TableCell
-                  align="center"
-                  sx={{ display: 'flex', justifyContent: 'center' }}
-                > 
-                <Link to={'/rolesform'}>
-                <ModeEditIcon sx={{ cursor: 'pointer', marginRight: 2 }} 
-                   onClick={() => editRoleHandler(row)} />
-                </Link>
-                  
-                  <DeleteIcon sx={{ cursor: 'pointer' }}
-                    onClick={() => deleteRoleHandler(row.role)} 
-             />
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            <DeleteIcon
+              sx={{ cursor: 'pointer' }}
+              onClick={() => deleteRoleHandler(row.role)}
+            />
+          </TableCell>
+        </TableRow>
+      ))}
+    </TableBody>
+  </Table>
+</TableContainer>
+
     
     </div>
     
